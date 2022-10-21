58fd9a4d5c34bfd5d182486e6292936fe3b3dc06
vandersonbfs <vanderson_b@hotmail.com>
2022-10-21 11:26:08 -0300

    Aula 19 - Comentarios

--- Faculdade/src/App.java
+++ Faculdade/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        /**
+         * @author Vanderson Batista
+         * 
+         */
+
+        // Aula 19 - Comentarios
+
+        /*
+         * Comentario com varias linhas
+         * 
+         * teste de comentarios
+         */
+
     }
 }
