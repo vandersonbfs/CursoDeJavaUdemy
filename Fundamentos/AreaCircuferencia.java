a0a4e9b6dc92f2f710311bd694e456fcfd20fa7f
vandersonbfs <vanderson_b@hotmail.com>
2022-10-21 11:45:51 -0300

    Aula 20 - Variaveis e Constantes

---
+++ Fundamentos/AreaCircuferencia.java
@@ -0,0 +1,14 @@
+public class AreaCircuferencia {
+    public static void main(String[] args) {
+        /**
+         * @autor Vanderson Batista
+         */
+        double raio = 3.4;
+        final double PI = 3.14159; // pi não é varial ela é constante, com o comando final antes.
+        // Colocar o nome da constante em maiusculo.
+        double area = PI * raio * raio;
+
+        System.out.println("Área = " + area);
+
+    }
+}
