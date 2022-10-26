9a7298c3327aed3de0a6b706a2266b7e03f6f6c1
vandersonbfs <vanderson_b@hotmail.com>
2022-10-26 16:03:13 -0300

    Aula 31 PrimitivoVsObjeto

---
+++ Fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,17 @@
+public class PrimitivoVsObjeto {
+    public static void main(String[] args) {
+        /*
+         * Java tem 8 tipos Primitivos
+         * byte;
+         * short;
+         * int;
+         * long;
+         * boolean;
+         * char;
+         * float; e.
+         * double.
+         * Wrappers são a versão objeto dos tipos primitivos
+         */
+
+    }
+}
