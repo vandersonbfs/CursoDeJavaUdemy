e9bce064bb19ba03ce1c1cd2555e54acde15f577
vandersonbfs <vanderson_b@hotmail.com>
2022-10-26 10:38:02 -0300

    Aula 27 Import

---
+++ Fundamentos/Import.java
@@ -0,0 +1,23 @@
+
+public class Import {
+
+    import java.util.Date;
+
+    // import javax.swing.JButton;
+
+    public static void main(String[] args) {
+        // Aula 27 Import
+        // O import fica fora da classe.
+
+
+        String s = "Bom dia!";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+        //JButton botao = new JButton();
+
+
+    }
+}
