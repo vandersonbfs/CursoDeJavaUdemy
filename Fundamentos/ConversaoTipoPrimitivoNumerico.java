a5f2b668f4d067298f2e21bb8b284377c48521e2
vandersonbfs <vanderson_b@hotmail.com>
2022-10-27 17:28:27 -0300

    Aula 34 Conversao implicita

---
+++ Fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,20 @@
+public class ConversaoTipoPrimitivoNumerico {
+    public static void main(String[] args) {
+
+        double a = 1; // Conversao implicita
+        System.out.println(a);
+
+        // Fazendo o casting
+        float b = (float) 1.123; // Conversao explicita (CAST)
+        System.out.println(a);
+
+        int c = 340;
+        byte d = (byte) c; // Conversao explicita (CAST)
+        System.out.println(d);
+
+        double e 1.99999;
+        int f (int) e;
+        System.out.println(f);
+
+     }
+}
