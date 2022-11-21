0fb8ccb96eac2b78b6d98797e198f69e9e3d5a1a
vandersonbfs <vanderson_b@hotmail.com>
2022-11-21 09:57:40 -0300

    Alua 35 Conversão Número para String

--- Fundamentos/ConversaoNumeroString.java
+++ Fundamentos/ConversaoNumeroString.java
@@ -1,3 +1,18 @@
 public class ConversaoNumeroString {
+    public static void main(String[] args) {
+        // Alua 35 Conversão Número para String
+
+        Integer num1 = 10000;
+
+        // Convertendo para String:
+        System.out.println(num1.toString());
+        // Contar os caracteres:
+        System.out.println(num1.toString().length());
+
+        // Convertendo usando o tipo:
+        int num2 = 100000;
+        System.out.println(Integer.toString(num2));
+
+    }
 
 }
