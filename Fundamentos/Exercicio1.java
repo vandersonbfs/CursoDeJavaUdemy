7454d3cf01e6fd441b6a3ec2d558b47ec3e71907
vandersonbfs <vanderson_b@hotmail.com>
2022-11-29 11:13:56 -0300

    Exercicio1

---
+++ Fundamentos/Exercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        /**
+         * 1. Criar um programa que leia a temperatura em Celsius e converta para
+         * Fahrenheit.
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double conversao = (celsius - 32) / 1.8;
+
+        System.out.print("Valor em Farenheit: " + conversao);
+
+        scanner.close();
+    }
+}
