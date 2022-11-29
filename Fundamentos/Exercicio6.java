21656e4c5630782a1a6db38087c8772e0307090d
vandersonbfs <vanderson_b@hotmail.com>
2022-11-29 12:57:51 -0300

    Exercicios

--- Fundamentos/Exercicio6.java
+++ Fundamentos/Exercicio6.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Exercicio6 {
     public static void main(String[] args) {
-
+        /**
+         * Criar um programa que resolve equações do segundo grau (ax2 + bx + c = 0)
+         * utilizando a fórmula de Bhaskara. Use como exemplo a = 1, b = 12 e c = -13.
+         * Encontre o delta
+         */
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Equação: ax² + bx + c = 0");
