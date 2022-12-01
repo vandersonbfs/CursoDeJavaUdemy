49ae451e2df89a1f7bc52b3223860e0fea8e4bb5
vandersonbfs <vanderson_b@hotmail.com>
2022-12-01 09:29:44 -0300

    Aula 62 Condicional IF

---
+++ Condicionais/If.java
@@ -0,0 +1,23 @@
+package Condicionais;
+
+import java.util.Scanner;
+
+public class If {
+    public static void main(String[] args) {
+
+        // Aula 62 Condicional IF
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Informe a média: ");
+        double media = entrada.nextDouble();
+
+        if (media <= 10 && media >= 7.0) {
+            System.out.println("Aprovado!");
+            System.out.println("Parabéns!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+        entrada.close();
+
+    }
+}
