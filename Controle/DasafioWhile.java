2a1d8a2adc7cf340b3b580e74a0ee252d5de51c9
vandersonbfs <vanderson_b@hotmail.com>
2022-12-05 16:56:00 -0300

    Aula 74 Desafio While

---
+++ Controle/DasafioWhile.java
@@ -0,0 +1,27 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class DasafioWhile {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int quantidadeDeNotas = 0;
+        double nota = 0;
+        double total = 0;
+
+        while (nota != -1) {
+            System.out.println("Informe a nota:");
+            nota = entrada.nextDouble();
+
+            if (nota <= 10 && nota >= 0) {
+                total += nota;
+                quantidadeDeNotas++;
+
+            }
+
+        }
+        double media = total / quantidadeDeNotas;
+        System.out.println("Mèdia = " + media);
+        entrada.close();
+    }
+}
