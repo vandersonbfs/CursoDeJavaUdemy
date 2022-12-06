5b0589fa1119fdb303a24f90e75f5fe6cbac0843
vandersonbfs <vanderson_b@hotmail.com>
2022-12-06 10:12:50 -0300

    Coloquei o break

--- Controle/SwitchSemBreak.java
+++ Controle/SwitchSemBreak.java
@@ -7,20 +7,27 @@ public class SwitchSemBreak {
         switch (faixa.toLowerCase()) {
             case "preta":
                 System.out.println("Sei o Bassai-Dai...");
+                break;
             case "marrom":
                 System.out.println("Sei o Tekki Shodan...");
+                break;
             case "roxa":
                 System.out.println("Sei o Heian Godan...");
+                break;
             case "verde":
                 System.out.println("Sei o Heian Yodan...");
+                break;
             case "laranja":
                 System.out.println("Sei o Heian Sandan...");
+                break;
             case "vermelha":
                 System.out.println("Sei o Heian Nidan...");
+                break;
             case "amarela":
                 System.out.println("Sei o Heian Shodan...");
-                // default:
-                // System.out.println("Não sei nada...");
+                break;
+            default:
+                System.out.println("Não sei nada...");
         }
     }
 }
