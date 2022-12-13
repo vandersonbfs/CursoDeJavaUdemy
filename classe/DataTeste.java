58f2d04e8d213ab50bbcdb1af66dff02e9c9ae36
vandersonbfs <vanderson_b@hotmail.com>
2022-12-13 08:14:25 -0300

    Aula 94 Desafio Classe Data

---
+++ classe/DataTeste.java
@@ -0,0 +1,21 @@
+package classe;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class DataTeste {
+
+    /**
+     * Data d1 = new Data();
+     * d1.dia = 7;
+     * d1.mes = 11;
+     * d1.ano = 2021;
+     * 
+     * Data d2 = new Data();
+     * d2.dia = 31;
+     * d2.mes = 12;
+     * d2.ano = 2020;
+     * 
+     * System.out.printf("%d/%d/%d/\n", d1.dia, d1.mes, d1.ano);
+     **/
+
+}
