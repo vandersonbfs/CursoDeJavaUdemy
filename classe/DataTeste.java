6a473d232d7f154babaaaa10e04060abef9c4447
vandersonbfs <vanderson_b@hotmail.com>
2022-12-19 16:09:56 -0300

    Aula 101. Desafio Construtor

--- classe/DataTeste.java
+++ classe/DataTeste.java
@@ -4,14 +4,18 @@ public class DataTeste {
     public static void main(String[] args) {
         FormatoData d1 = new FormatoData();
 
-        d1.dia = 7;
-        d1.mes = 11;
-        d1.ano = 2021;
+        /**
+         * 1.dia = 7;
+         * d1.mes = 11;
+         * d1.ano = 2021;
+         **/
 
-        var d2 = new FormatoData();
-        d2.dia = 31;
-        d2.mes = 12;
-        d2.ano = 2020;
+        var d2 = new FormatoData(31, 12, 2020);
+        /**
+         * d2.dia = 31;
+         * d2.mes = 12;
+         * d2.ano = 2020;
+         **/
 
         System.out.println(d1.obterDataFormatada());
         System.out.println(d2.obterDataFormatada());
--- classe/FormatoData.java
+++ classe/FormatoData.java
@@ -6,6 +6,18 @@ public class FormatoData {
     int mes;
     int ano;
 
+    FormatoData() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    FormatoData(int diaInicial, int mesInicial, int anoInicial) {
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+    }
+
     String obterDataFormatada() {
         return String.format("%d/%d/%d", dia, mes, ano);
     }
