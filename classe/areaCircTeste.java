3dec0b46df42ea9f01034d639a0fede76289b277
vandersonbfs <vanderson_b@hotmail.com>
2022-12-27 15:22:15 -0300

    Aula 110. Desafio Primeiro Trauma

---
+++ classe/areaCircTeste.java
@@ -0,0 +1,9 @@
+package classe;
+
+public class areaCircTeste {
+    public static void main(String[] args) {
+        AreaCirc a = new AreaCirc(5.6);
+        a.pi = 10;
+        System.out.println(a.area());
+    }
+}
