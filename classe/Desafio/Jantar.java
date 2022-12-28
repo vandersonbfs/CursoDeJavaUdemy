29e2ced0e8ca1bd705c05c37a22b5292f7cbe54e
vandersonbfs <vanderson_b@hotmail.com>
2022-12-28 13:17:12 -0300

    Aula 116. Desafio Módulo

---
+++ classe/Desafio/Jantar.java
@@ -0,0 +1,20 @@
+package classe.Desafio;
+
+public class Jantar {
+    public static void main(String[] args) {
+        Comida c1 = new Comida("Arroz", 0.180);
+        Comida c2 = new Comida("Feijão", 0.300);
+
+        Pessoa p = new Pessoa("Joao", 99.8);
+
+        System.out.println(p.apresentar());
+
+        p.comer(c1);
+
+        System.out.println(p.apresentar());
+
+        p.comer(c2);
+
+        System.out.println(p.apresentar());
+    }
+}
